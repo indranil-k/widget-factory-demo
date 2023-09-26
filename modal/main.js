211da633608b46c9e833a95874fdc8e0d1237cca
indranil-k <indranil.karmarkar@merklecxm.com>
2023-09-26 16:47:03 +0530

    updated changes

--- modal/main.js
+++ modal/main.js
@@ -40,12 +40,14 @@ $.widget("custom.modal", {
 
     // Set initial title and content
     this.modal.find(".modal-title").text(this.options.modalTitle);
-    this.modal.find(".modal-content").text(this.options.modalContent);
+    // this.modal.find(".modal-content").text(this.options.modalContent);
+    this.modal.find(".modal-content").html(this.options.modalContent);
 
     // Allow the widget to be initialized in an open or closed state
     if (this.options.isOpen) {
       this.open();
     }
+
   },
 
   open: function () {
@@ -58,75 +60,3 @@ $.widget("custom.modal", {
     this.modal.hide();
   }
 });
-
-
-  $.widget("custom.modalWithColorPicker", $.custom.modal, {
-      options: {
-        colorPickerId: "colorPicker", // ID for the color picker input field
-        selectedColor: "#ff0000", // Default selected color
-      },
-    
-      _create: function () {
-        // Call the parent _create function
-        this._super();
-    
-        // Add color picker input field to the modal content
-        this.colorPickerInput = $("<input>", {
-          id: this.options.colorPickerId,
-          type: "color",
-          value: this.options.selectedColor,
-        }).appendTo(this.modal.find(".modal-content"));
-    
-        // Add a button to open the color picker
-        this.colorPickerButton = $("<button>", {
-          text: "Pick a Color",
-          class: "color-picker-button",
-        }).appendTo(this.modal.find(".modal-content"));
-    
-        var self = this;
-    
-        // Event handler to update the selected color
-        this.colorPickerInput.on("input", function () {
-          var selectedColor = $(this).val();
-          self.options.selectedColor = selectedColor;
-        });
-    
-        // Event handler to open the color picker dialog
-        this.colorPickerButton.on("click", function () {
-          self.openColorPicker();
-        });
-      },
-    
-      openColorPicker: function () {
-        // Trigger a click event on the color picker input field
-        $("#" + this.options.colorPickerId).trigger("click");
-      },
-    
-      open: function () {
-        // Call the parent open function
-        this._super();
-    
-        // Show the color picker input field and button
-        this.colorPickerInput.show();
-        this.colorPickerButton.show();
-      },
-    
-      close: function () {
-        // Call the parent close function
-        this._super();
-    
-        // Hide the color picker input field and button
-        this.colorPickerInput.hide();
-        this.colorPickerButton.hide();
-      },
-  });
-    
-  // Usage
-      var modalWithColorPicker = $("#modalWithColorPicker").modalWithColorPicker({
-      modalTitle: "Modal with Color Picker",
-      modalContent: "Pick a color:",
-      isOpen: false, // Set to true to open the modal by default
-  });
-  
-  // Open the modal with color picker
-  modalWithColorPicker.modalWithColorPicker("open");
---
+++ spinnerNew/main.js
@@ -0,0 +1,114 @@
+$.widget("custom.customSpinner", $.ui.spinner, {
+    options: {
+        disabled: false, //Disables the spinner if set to true.
+        min: 0, //Specifies the minimum allowed value for the spinner.
+        max: 90, //Specifies the maximum allowed value for the spinner.
+        numberFormat: "C", //Defines the format for displaying numbers.
+        page: 2, //Determines the number of steps to change when using the page up/page down keys.
+        icons: {
+            upIcon: "+", //Allows customization of the increament icon displayed in the spinner.
+            downIcon: "-", //Allows customization of the decrement icon displayed in the spinner.
+        },
+        create: function(event, ui) {//Callback before spinner loads.
+        },
+        spin: function(event, ui) { //Callback when the user increments or decrements the value.
+        },
+        stop: function(event, ui) { //Callback when the user stops spinning the value.
+        },
+        change: function(event, ui) { //Callback when the value of the spinner changes.
+        },
+        culture: "en-US", //Specifies the culture (locale) for formatting numbers.
+        step: 1, //Sets the increment or decrement step for the spinner.
+        initialValue: 0, //Allows to set initiall value in the spinner. It should be greater then min value
+        allowNegative: false
+    },
+
+    _create: function() {
+        this.element.addClass('wf-spinner-input');
+        setTimeout(function() {
+            var parentElement = this.element.closest('span');
+            parentElement.addClass('wf-spinner');
+
+            parentElement.find('.ui-spinner-up .ui-button-icon').text(this.options.icons.upIcon);
+            parentElement.find('.ui-spinner-down .ui-button-icon').text(this.options.icons.downIcon);
+        }.bind(this), 0);
+
+        if(this.options.initialValue < this.options.min && !this.options.allowNegative) {
+            this.element.val(this.options.min);
+        } else {
+            this.element.val(this.options.initialValue);
+        }
+
+        this._super();
+    },
+});
+
+$.widget("custom.customTimer", $.custom.customSpinner, {
+    options: {
+        expandable: true,
+        endTime: 0,
+        timerDelay: 1000,
+    },
+
+    _create: function () {
+        this.element.addClass('wf-timer');
+        this._startTimer();
+    },
+
+    _startTimer: function () {
+        var self = this;
+        var timerValue = self.options.max;
+
+        var timerContainer = $('.timer-container');
+        var timerCounter = $("<div>").addClass("timer-counter").appendTo(timerContainer);
+        var timerPlayButton = $("<button>").addClass("timerPlayButton").text('Pause').appendTo(timerContainer);
+        var timerminus = $("<span>").addClass("timer-minus").appendTo(timerCounter);
+        var timerminutes = $("<span>").addClass("timer-minutes").text('00').appendTo(timerCounter);
+        var timerseparator = $("<span>").addClass("timer-separator").text(':').appendTo(timerCounter);
+        var timersecondsContainer = $("<div>").addClass("timer-seconds-container").appendTo(timerCounter);
+        var timerseconds = $("<span>").addClass("timer-seconds").text('00').appendTo(timersecondsContainer);
+
+        var timerInterval;
+
+        function updateTimer() {
+            var isNegative = timerValue < 0;
+
+            if (!self.options.allowNegative && isNegative) {
+                timerValue = 0;
+            }
+
+            var absTimerValue = Math.abs(timerValue);
+            var seconds = absTimerValue % 60;
+            var minutes = Math.floor(absTimerValue / 60);
+
+            isNegative ? $(timerminus).text("-") : $(timerminus).text("");
+            $(timerminutes).text(minutes.toString().padStart(2, '0'));
+            $(timerseconds).text(seconds.toString().padStart(2, '0'));
+
+            self._trigger("change", null, { value: timerValue });
+
+            if (timerValue <= self.options.min) {
+                clearInterval(timerInterval);
+                timerPlayButton.text('Reset');
+            }
+
+            timerValue -= self.options.step;
+        }
+
+        timerPlayButton.on("click", function () {
+            if (timerPlayButton.text() === "Pause") {
+                clearInterval(timerInterval);
+                timerPlayButton.text("Play");
+            } else if (timerPlayButton.text() === "Reset") {
+                timerValue = self.options.max;
+                timerInterval = setInterval(updateTimer, self.options.timerDelay);
+                timerPlayButton.text('Pause');
+            } else {
+                timerInterval = setInterval(updateTimer, self.options.timerDelay);
+                timerPlayButton.text("Pause");
+            }
+        });
+
+        timerInterval = setInterval(updateTimer, self.options.timerDelay);
+    },
+});
